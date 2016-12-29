8284da0b4b52813cc528658abb5976f108e9cec3
Louis_Huang <ar.htc.acc@gmail.com>
2016-12-29 17:53:48 +0800

    Forever: re-starts when server crashes. Remove Express 'x-powered-by' header.

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// X-Powered-By: Express (default)
+app.disable('x-powered-by');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
